0a934d96bf5102f719920102c169c1f2427018ae
LeoLeto <132474553+LeoLeto@users.noreply.github.com>
2024-08-15 21:32:41 -0300

    transparent-background working base

--- src/functions/removeImageBackground.ts
+++ src/functions/removeImageBackground.ts
@@ -6,7 +6,11 @@ import {
 } from "@azure/functions";
 import { removeBackground } from "@imgly/background-removal-node";
 import path = require("path");
-import * as fs from "fs";
+// import * as fs from "fs";
+import sharp = require("sharp");
+import { Rembg } from "@xixiyahaha/rembg-node";
+import * as fs from "fs/promises";
+import { transparentBackground } from "transparent-background";
 
 let image_src: //  ImageData |
 ArrayBuffer | Uint8Array | Blob | URL | string = path.resolve("./r3.png");
@@ -15,7 +19,20 @@ export async function removeImageBackground(
   request: HttpRequest,
   context: InvocationContext
 ): Promise<HttpResponseInit> {
-    main()
+  const input = await fs.readFile("./r3.png");
+  console.log("input: ", input);
+
+  const output = await transparentBackground(input, "png", {
+    // uses a 1024x1024 model by default
+    // enabling fast uses a 384x384 model instead
+    fast: false,
+  });
+  console.log("output: ", output);
+
+  await fs.writeFile("test-output.png", output);
+  return;
+
+  // main();
   //   const blob = await removeBackground(image_src);
   //   console.log("this runs");
   //   console.log("blob: ", blob);
@@ -52,26 +69,26 @@ async function removeImageBackgroundTEST(imgSource) {
 }
 
 // Example usage
-async function main() {
-  try {
-    // Path to the input image
-    const imgSource = "./r3.png";
+// async function main() {
+//   try {
+//     // Path to the input image
+//     const imgSource = "./r3.png";
 
-    // Removing background from the input image
-    const resultDataURL = await removeImageBackgroundTEST(imgSource);
+//     // Removing background from the input image
+//     const resultDataURL = await removeImageBackgroundTEST(imgSource);
 
-    // Writing the result to a file (optional)
-    fs.writeFileSync("output.png", resultDataURL.split(";base64,").pop(), {
-      encoding: "base64",
-    });
+//     // Writing the result to a file (optional)
+//     fs.writeFileSync("output.png", resultDataURL.split(";base64,").pop(), {
+//       encoding: "base64",
+//     });
 
-    // Logging success message
-    console.log("Background removed successfully.");
-  } catch (error) {
-    // Logging error message
-    console.error("Error:", error.message);
-  }
-}
+//     // Logging success message
+//     console.log("Background removed successfully.");
+//   } catch (error) {
+//     // Logging error message
+//     console.error("Error:", error.message);
+//   }
+// }
 
 app.http("removeImageBackground", {
   methods: ["GET", "POST"],
